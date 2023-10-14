a4ea883b8cd6bfdef51ccc49737e760c6527d5c7
pranjal1006 <60665945+pranjal1006@users.noreply.github.com>
2023-10-15 01:39:01 +0530

    fix deploy

--- set-matrix-zero/frontend/vanilla-app/api/v1/zeroes/process/handler.js
+++ set-matrix-zero/frontend/vanilla-app/api/v1/zeroes/process/handler.js
@@ -1,12 +1,42 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
 export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
+  request,
+  response,
 ) {
   response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+    body: JSON.stringify({ outputMatrix }),
   });
+}
+
+function setZeroes (matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let rowZeroIndx = [];
+  let colZeroIndx = [];
+  
+  const map = {};
+  
+  const getAllIndicesOfZero = (row) => {
+    return  row.map((e, i) => e === 0 ? i : '').filter(String)
+  }
+  for(let i=0;i<m;i++){
+         map[i] = matrix[i];
+  }
+ 
+  for(let key in map){
+      if(map[key].includes(0)){
+          rowZeroIndx.push(+key);
+          colZeroIndx.push(...getAllIndicesOfZero(map[key]));
+      }
+  }
+  for(let i of rowZeroIndx){
+      for(let k=0;k<n;k++){
+          matrix[i][k] = 0;
+      }
+  }
+  for(let i of colZeroIndx){
+      for(let k=0;k<m;k++){
+          matrix[k][i] = 0;
+      }
+  }
+  return matrix;
 }
